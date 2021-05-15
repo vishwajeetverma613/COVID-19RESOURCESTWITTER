d3f65b003677443b66e4c1f32e4a325238a33778
vishwajeetverma613 <51289719+vishwajeetverma613@users.noreply.github.com>
2021-05-15 11:32:25 +0530

    Update twit.js

--- twit.js
+++ twit.js
@@ -106,20 +106,5 @@ let pages = null;
 
 
 
-async function autoScroll(page){
-    await page.evaluate(async () => {
-        await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 100;
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-                if(totalHeight >= scrollHeight){
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 100);
-        });
-    });
+
 }
